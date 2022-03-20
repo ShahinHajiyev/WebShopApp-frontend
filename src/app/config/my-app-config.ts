cdfb14dc11420630957e09aae239faaecf2a1d25
ShahinHajiyev <hshahin.t401@code.edu.az>
2022-03-20 19:46:21 +0100

    openssl configuration changes made.

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa475uh5jbV5N12W5d7',
         issuer: 'https://dev-83661162.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
