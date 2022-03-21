7ef5b374e904e63d4e59d5c6012afea93b5259e0
ShahinHajiyev <hshahin.t401@code.edu.az>
2022-03-21 23:22:40 +0100

    0.01 amount lack bug is fixed. Math.round is implemented to basically convert js float to int.

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -273,7 +273,7 @@ export class CheckoutComponent implements OnInit {
     purchase.orderItems = orderItems;
 
     //compute payment info
-    this.paymentInfo.amount = this.totalPrice * 100;
+    this.paymentInfo.amount = Math.round(this.totalPrice * 100);
     this.paymentInfo.currency = "USD";
 
     console.log(`this.paymentInfo.amount: ${this.paymentInfo.amount}`);
